36bf6bb9f5b29536b2eb0984ceb0aee5f4694c85
clintonjosephs <clintonjosephs26@gmail.com>
2022-03-21 13:47:56 +0100

    created animals table

--- data.sql
+++ data.sql
@@ -3,3 +3,5 @@
 INSERT INTO animals (name) VALUES ('Luna');
 INSERT INTO animals (name) VALUES ('Daisy');
 INSERT INTO animals (name) VALUES ('Charlie');
+
+
--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals 
+     ( id INTEGER GENERATED ALWAYS AS IDENTITY, 
+      name VARCHAR(250), 
+      date_of_birth DATE, 
+      escape_attempts INTEGER, 
+      neutered BIT, 
+      weight_kg DECIMAL );
