16101f102243978b99b21175187acdbcfbcbdf5c
clintonjosephs <clintonjosephs26@gmail.com>
2022-03-21 14:06:16 +0100

    added data of four animals to database

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals 
-     ( id INTEGER GENERATED ALWAYS AS IDENTITY, 
-      name VARCHAR(250), 
-      date_of_birth DATE, 
-      escape_attempts INTEGER, 
-      neutered BIT, 
-      weight_kg DECIMAL );
+            ( id INTEGER GENERATED ALWAYS AS IDENTITY, 
+              name VARCHAR(250), date_of_birth DATE, 
+              escape_attempts INTEGER, 
+              neutered BOOLEAN, 
+              weight_kg DECIMAL
+            );
