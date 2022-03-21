008d3aa933aac643c59998ab986e21794f6f047a
clintonjosephs <clintonjosephs26@gmail.com>
2022-03-21 14:20:37 +0100

    added query to fetch date_of_birth of agumon and pikachu

--- queries.sql
+++ queries.sql
@@ -5,5 +5,7 @@ SELECT * from animals WHERE name LIKE '%mon';
 SELECT name from animals WHERE 
       EXTRACT(YEAR FROM date_of_birth) >= 2016 
       AND EXTRACT(YEAR FROM date_of_birth) <= 2019;
-      
+
 SELECT name from animals WHERE neutered = TRUE AND escape_attempts < 3;
+
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';                                                                                                           ;
