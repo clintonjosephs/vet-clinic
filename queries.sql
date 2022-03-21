75f6a2585883135e2fba8634f27d1c63d367d19b
clintonjosephs <clintonjosephs26@gmail.com>
2022-03-21 14:17:04 +0100

    added queries to fetch animal name based on born date, neutered and escape attemps

--- queries.sql
+++ queries.sql
@@ -1,3 +1,9 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+SELECT * from animals WHERE name LIKE '%mon';
+
+SELECT name from animals WHERE 
+      EXTRACT(YEAR FROM date_of_birth) >= 2016 
+      AND EXTRACT(YEAR FROM date_of_birth) <= 2019;
+      
+SELECT name from animals WHERE neutered = TRUE AND escape_attempts < 3;
