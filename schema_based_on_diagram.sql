81a22efd6928184ea18a5a75fc355133ed6264ae
uae <aniekaudo@yahoo.com>
2022-03-29 13:26:40 +0100

    :card_file_box: Indexes
    Add indexes to foreign keys on clinic database

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -87,3 +87,10 @@ CREATE DATABASE clinic;
             ON DELETE CASCADE
     );
 /* END */
+
+CREATE INDEX  patient_id_index ON medical_histories(patient_id);
+CREATE INDEX  invoice_id_index ON invoice_items(invoice_id);
+CREATE INDEX  treatment_id_index ON invoice_items(treatment_id);
+CREATE INDEX  medical_id_index ON invoices(medical_history_id);
+CREATE INDEX  treatment_id_history_index ON treatments_history(treatment_id);
+CREATE INDEX  medical_history_id_index ON treatments_history(medical_history_id);
